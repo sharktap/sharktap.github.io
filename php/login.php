4aed86ce86935cc6c14706a8b53a1a7e26d75c96
sharktap <ministud.info@gmail.com>
2024-08-30 11:45:50 +0900

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = "username"; // Mysql username 
+$db_password = "password"; // Mysql password 
+$db_name = "database"; // Database name 
+$db_table = "scores"; // Table name
+
+?>
